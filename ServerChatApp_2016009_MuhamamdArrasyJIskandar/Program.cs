5559c3b5e3bec7e1fefae176001a9cd5c6ef8238
Arrasy <arrasyjunior@gmail.com>
2021-01-15 01:56:42 +0700

    Add project files.

---
+++ ServerChatApp_2016009_MuhamamdArrasyJIskandar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFService_2Way_2016009;
+
+namespace ServerChatApp_2016009_MuhamamdArrasyJIskandar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallBack));
+                hostObjek.Open();
+                Console.WriteLine("Server ON, Ready to Use");
+                Console.ReadLine();
+                hostObjek.Close();
+
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
